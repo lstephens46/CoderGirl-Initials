0313e3cfb3ae5d1a23b367c7af080c99e8177c5c
Lauren Stephens <9128505+lstephens46@users.noreply.github.com>
2019-03-12 17:21:56 -0500

    messing around

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
-
+using System.Linq;
 namespace Initials
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
+            
+            Console.WriteLine(Initials("Lauren Faye Stephens"));
+
+
             Console.ReadLine();
         }
 
+        
         // TODO: Create a method that takes in a full name and return the initials in all capital letters.
+        public static string Initials(String name)
+        {
+            
+            string firstLetters = String.Join(String.Empty, text.Split(new[] { ' ' }).Select(word => word.First()))
+
+            foreach (var name in name.split(' '))
+            {
+                firstLetters += part.substring(0, 1);
+            }
+            firstLetters.ToUpper();
+            return firstLetters;
+
+            
+            string firstLetters =
+                String.Join(String.Empty, text.Split(new[] { ' ' }).Select(word => word.First()))
+
+
+
+
+
+        }
+
     }
+
 }
---
+++ MypracticeInitials/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Initials
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.Write("Please enter your first name: ");
+            //string firstName = Console.ReadLine();
+
+            //Console.Write("Please enter your middle name: ");
+            //string middleName = Console.ReadLine();
+
+
+            //Initials(lauren, faye, stephens)
+            Console.WriteLine(Name(firstName));
+
+
+            Console.ReadLine();
+        }
+
+        public static string Name(string typeOfName)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Please enter your " + typeOfName + ": ");
+                string name = Console.ReadLine();
+                return name;
+            }
+            //Console.Write("Please enter your " + typeOfName + ": ");
+            //string name = Console.ReadLine();
+            //return name;
+        }
+        // TODO: Create a method that takes in a full name and return the initials in all capital letters.
+        public static string Initials(string firstName, string middleName, string lastName)
+        {
+
+
+        }
+
+    }
+
+}
