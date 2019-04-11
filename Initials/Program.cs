4f32a5d8c0ab0a02f2dc8e04e00d189af1b06b19
L Stephens <lstephens46@my.stlcc.edu>
2019-04-11 14:49:29 -0500

    finish and pass all tests

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -7,7 +7,7 @@ namespace Initials
         public static void Main(string[] args)
         {
             
-            Console.WriteLine(Initials("Lauren Faye Stephens"));
+            Console.WriteLine(Initials("Ln Sy Pt"));
 
 
             Console.ReadLine();
@@ -17,19 +17,14 @@ namespace Initials
         // TODO: Create a method that takes in a full name and return the initials in all capital letters.
         public static string Initials(String name)
         {
-            
-            string firstLetters = String.Join(String.Empty, text.Split(new[] { ' ' }).Select(word => word.First()))
 
-            foreach (var name in name.split(' '))
-            {
-                firstLetters += part.substring(0, 1);
-            }
-            firstLetters.ToUpper();
-            return firstLetters;
+            string firstLetters = String.Join(String.Empty, name.Split(new[] { ' ' }).Select(word => word.First()));
+
+          
+            return firstLetters.ToUpper();
 
             
-            string firstLetters =
-                String.Join(String.Empty, text.Split(new[] { ' ' }).Select(word => word.First()))
+            
 
 
 
